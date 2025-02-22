745f49fcda35dadb7712d6e767885949abf7256c
19Doors <sakaarsri1904@gmail.com>
2025-02-23 00:24:02 +0530

    Init React Done

---
+++ src/electron/main.js
@@ -0,0 +1,17 @@
+import { app, BrowserWindow } from "electron"
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('dist/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+})
