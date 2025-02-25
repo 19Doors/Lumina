ae31754e328307976629c6f342a2da35ebff65e5
19Doors <sakaarsri1904@gmail.com>
2025-02-25 17:06:48 +0530

    Added shadCN Support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
     react(), 
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   base: "./",
   server: {
     headers: {
